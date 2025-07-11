3542bc7d1e68bd7ddbeff281db81b0e4b79116f7
Harsh Tripathi <harshtripathiwell@gmail.com>
2025-07-11 18:51:15 +0530

    index.js

--- index.js
+++ index.js
@@ -210,8 +210,8 @@ app.use("/api/auth", authroute);
 // app.use("/api/papers", require("./routes/paperRoutes"));
 
 // Start server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 module.exports=app;
